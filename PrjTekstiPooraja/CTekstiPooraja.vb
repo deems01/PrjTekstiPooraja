3b0573972bcfda23dc9f1d8751859c4d9fe3eff2
Uku-Erik Hindrikson <ukuerik.hindrikson@gmail.com>
2024-02-07 16:14:02 +0200

    Private

--- PrjTekstiPooraja/CTekstiPooraja.vb
+++ PrjTekstiPooraja/CTekstiPooraja.vb
@@ -1,3 +1,38 @@
 ﻿Public Class CTekstiPooraja
+    Implements ITeisendused
 
+    Private Property intAlgus As Integer Implements ITeisendused.intAlgus
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Integer)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Private Property intLopp As Integer Implements ITeisendused.intLopp
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Integer)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Private Property strTekst As String Implements ITeisendused.strTekst
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As String)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Private Sub teisendaTekst(ByRef strSisendTekst As String) Implements ITeisendused.teisendaTekst
+        Throw New NotImplementedException()
+    End Sub
+
+    Private Function pooraTeks() As String Implements ITeisendused.pooraTeks
+        Throw New NotImplementedException()
+    End Function
 End Class
