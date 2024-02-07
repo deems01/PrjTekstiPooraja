e10d0a25ce82638568121e6df6020ec5ae745899
Uku-Erik Hindrikson <ukuerik.hindrikson@gmail.com>
2024-02-07 16:56:30 +0200

    wip

--- PrjKonteiner/formKasutajaAken.vb
+++ PrjKonteiner/formKasutajaAken.vb
@@ -2,4 +2,11 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub btnPoora1_Click(sender As Object, e As EventArgs) Handles btnPoora1.Click
+        Dim pooraja As PrjTekstiPooraja.ITeisendused
+        pooraja = New PrjTekstiPooraja.CTekstiPooraja
+        pooraja.strTekst = txtSisendTekst.Text
+        txtValjundTekst1.Text = pooraja.pooraTekst()
+    End Sub
 End Class
