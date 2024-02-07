9f1ae9b473ac6b4ac6049e4c4d48ac9b1315bb47
Uku-Erik Hindrikson <ukuerik.hindrikson@gmail.com>
2024-02-07 17:02:13 +0200

    wip

--- PrjKonteiner/formKasutajaAken.vb
+++ PrjKonteiner/formKasutajaAken.vb
@@ -9,4 +9,11 @@
         pooraja.strTekst = txtSisendTekst.Text
         txtValjundTekst1.Text = pooraja.pooraTekst()
     End Sub
+
+    Private Sub btn2Poora_Click(sender As Object, e As EventArgs) Handles btn2Poora.Click
+        Dim pooraja As PrjTekstiPooraja.ITeisendused
+        pooraja = New PrjTekstiPooraja.CTekstiPooraja
+        pooraja.teisendaTekst(txtSisendTekst.Text)
+        txtValjundTekst2.Text = pooraja.strTekst
+    End Sub
 End Class
